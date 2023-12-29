f74e32f7d6e393eeedf87a57b556292d861d5e71
EL RUUUL3R ♛ <90483727+AyoubAlilou@users.noreply.github.com>
2023-12-29 17:48:29 +0100

    paper

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,73 @@
+import { Box, Typography, IconButton, Paper } from '@mui/material'
 import './Home.css'
 
 import React from 'react'
+import { Close } from '@mui/icons-material'
 
 function Home() {
   return (
     <div>
-      Homeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+      <Box>
+        <Paper
+          sx={{
+            position: 'relative',
+            width: '366px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            mt: '22px',
+            pt: '27px',
+            pb: '7px',
+          }}
+        >
+          <Typography sx={{ ml: '16px', fontSize: '1.3rem' }} variant='h6'>
+            GYM
+          </Typography>
+          <Typography
+            sx={{
+              mr: '33px',
+              fontWeight: 500,
+              fontSize: '1.4em',
+              opacity: '0.8',
+            }}
+            variant='h6'
+          >
+            $100
+          </Typography>
+          <IconButton sx={{ position: 'absolute', top: '0', right: '0' }}>
+            <Close fontSize='20px' />
+          </IconButton>
+        </Paper>
+
+        <Paper
+          sx={{
+            position: 'relative',
+            width: '366px',
+            display: 'flex',
+            justifyContent: 'space-between',
+            mt: '22px',
+            pt: '27px',
+            pb: '7px',
+          }}
+        >
+          <Typography sx={{ ml: '16px', fontSize: '1.3rem' }} variant='h6'>
+            GYM
+          </Typography>
+          <Typography
+            sx={{
+              mr: '33px',
+              fontWeight: 500,
+              fontSize: '1.4em',
+              opacity: '0.8',
+            }}
+            variant='h6'
+          >
+            $100
+          </Typography>
+          <IconButton sx={{ position: 'absolute', top: '0', right: '0' }}>
+            <Close fontSize='20px' />
+          </IconButton>
+        </Paper>
+      </Box>
     </div>
   )
 }
--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -14,6 +14,7 @@ function Root() {
           ml: `${drawerWidth}px`,
           display: 'flex',
           justifyContent: 'center',
+          mt: '66px',
         }}
       >
         <Outlet />
